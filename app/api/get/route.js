3be1978038a26f5f09138f629fa010312eacab13
Ehsan Saleem <ehsandesigner.s@gmail.com>
2025-05-06 05:07:56 +0530

    Update route.js

--- app/api/get/route.js
+++ app/api/get/route.js
@@ -3,7 +3,7 @@ import blogModel from '@/models/Blog'
 import { NextResponse } from 'next/server'
 
 export async function GET(req) {
-    await mongoose.connect("mongodb+srv://ehsan:ehsan2024@cluster0.vqrb8yl.mongodb.net/Inkwave?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect(process.env.MONGO_URI)
           console.log("Connected to database")
     const blogs = await blogModel.find()
     return NextResponse.json({
